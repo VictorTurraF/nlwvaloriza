85e8e15295a2c98e3df362e5847837217e18aa56
Victor Turra <victor.florencio@univem.edu.br>
2021-06-26 16:27:58 -0300

    Create entities custom repositorys

---
+++ src/repositories/ComplimentRepository.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Compliment } from "../entities/Compliment";
+
+@EntityRepository(Compliment)
+export class ComplimentRepository extends Repository<Compliment> {
+  /**
+   * Compliment repository implementation
+   */
+}
---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  /**
+   * User repository implementation
+   */
+}
