1e7b2074e6c838a9a495a9f6692d0003fd9034ec
Victor Turra <victor.florencio@univem.edu.br>
2021-07-09 23:22:27 -0300

    Add compliments controller integration tests

---
+++ src/tests/feature/Compliments.test.ts
@@ -0,0 +1,21 @@
+import { Database } from '../../database/Database'
+import { app } from '../../App'
+import request from 'supertest'
+
+describe('Compliments Controller', () => {
+  beforeAll(async () => {
+    await Database.setUpConnection()
+  })
+
+  it('should not be able to access if not authenticated', async () => {
+    const response = await request(app)
+      .post('/compliments')
+      .send({
+        tag_id: '42304720-06a1-49d7-bd6f-91ab544b477e',
+        user_receiver: '03b4b3d1-af3e-482e-bca4-163c66b52343',
+        message: 'Você é sensacional!'
+      })
+
+    expect(response.status).toBe(401)
+  })
+})
